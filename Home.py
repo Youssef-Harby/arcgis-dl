518cc60dd29e2e13e77bde30a74a2dd9a09b398d
Youssef-Harby <youssef_harby@yahoo.com>
2022-06-18 01:49:20 +0200

    init streamlit with simple download parm

--- Home.py
+++ Home.py
@@ -1,5 +1,31 @@
+from prometheus_client import generate_latest
 import streamlit as st
-import pandas as pd
-import numpy as np
+from arcgis_dl.arcgis_dl import config, get_services, get_layers, get_query, write_layer
+import re
 
 st.title('ArcGIS Server Downloader')
+arc_url = st.text_input('ArcGIS Server Url','https://www.esrs.wmich.edu/arcgis/rest/services/Saudi')
+arc_token_1 = st.text_input('ArcGIS Server Token')
+arc_token_2 = st.text_input('ArcGIS Server Token ALT')
+
+if st.button('Download'):
+    url = arc_url.rstrip('/')
+    if re.search('/[A-Z][A-Za-z]+Server/[^/]+$', url):
+        query = get_query(url)
+        if query is not None:
+            layer, layer_data, layer_format = query
+            write_layer(layer, layer_data, url, layer_format)
+    elif re.search('/[A-Z][A-Za-z]+Server$', url):
+        for layer_url in get_layers(url):
+            query = get_query(layer_url)
+            if query is not None:
+                layer, layer_data, layer_format = query
+                write_layer(layer, layer_data, layer_url, layer_format)
+    #elif re.search('/rest/services$', url):
+    else:
+        for service_url in get_services(url):
+            for layer_url in get_layers(service_url):
+                query = get_query(layer_url)
+                if query is not None:
+                    layer, layer_data, layer_format = query
+                    write_layer(layer, layer_data, layer_url, layer_format)
