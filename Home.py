ebb24979af801c9c2ee9a53cd08ba2613fd83712
Youssef-Harby <youssef_harby@yahoo.com>
2022-06-19 08:44:44 +0200

    add it to streamlit

--- Home.py
+++ Home.py
@@ -1,6 +1,7 @@
 from prometheus_client import generate_latest
 import streamlit as st
 from arcgis_dl.arcgis_dl import config, get_services, get_layers, get_query, write_layer
+from arcgis_dl.metadata import get_create_time, load_metadata, check_update, save_metadata
 import re
 
 st.title('ArcGIS Server Downloader')
@@ -9,7 +10,9 @@ arc_token_1 = st.text_input('ArcGIS Server Token')
 arc_token_2 = st.text_input('ArcGIS Server Token ALT')
 
 if st.button('Download'):
+    metadatas = load_metadata()
     url = arc_url.rstrip('/')
+    print(url)
     if re.search('/[A-Z][A-Za-z]+Server/[^/]+$', url):
         query = get_query(url)
         if query is not None:
@@ -21,11 +24,19 @@ if st.button('Download'):
             if query is not None:
                 layer, layer_data, layer_format = query
                 write_layer(layer, layer_data, layer_url, layer_format)
-    #elif re.search('/rest/services$', url):
+    # elif re.search('/rest/services$', url):
     else:
         for service_url in get_services(url):
-            for layer_url in get_layers(service_url):
-                query = get_query(layer_url)
-                if query is not None:
-                    layer, layer_data, layer_format = query
-                    write_layer(layer, layer_data, layer_url, layer_format)
+            create_time = get_create_time(service_url)  # get create time about this services
+            # if services data is change, downloading, else skipping
+            if check_update(service_url, create_time, metadatas):
+                for layer_url in get_layers(service_url):
+                    query = get_query(layer_url)
+                    if query is not None:
+                        layer, layer_data, layer_format = query
+                        write_layer(layer, layer_data, layer_url, layer_format)
+                # update metadata
+                metadatas[service_url] = create_time
+            else:
+                print('Skipping - service not update', service_url)
+        save_metadata(metadatas)  # save new metadata
