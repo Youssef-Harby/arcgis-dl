a1617cf8acb9d5ded00dbf272b88dc52b8f4ee0b
geoyee <geoyee@yeah.net>
2022-06-19 17:15:22 +0800

    feat(metadate): update

--- arcgis_dl/__main__.py
+++ arcgis_dl/__main__.py
@@ -1,7 +1,22 @@
 import argparse
 import re
 from .arcgis_dl import config, get_services, get_layers, get_query, write_layer
-from .metadata import get_create_time, load_metadata, check_update, save_metadata
+from .metadata import (
+    init_metadata, save_metadata, load_metadata, clear_metadata, \
+    get_date_time, check_update
+)
+
+
+def downloading(url, metadatas):
+    query = get_query(url)
+    if query is not None:
+        layer, layer_data, layer_format = query
+        data_time = get_date_time(url)
+        if check_update(url, data_time, metadatas):
+            write_layer(layer, layer_data, url, layer_format)
+            save_metadata({url: data_time})
+        else:
+            print('Skipping - no update:', url)
 
 
 def main():
@@ -30,33 +45,21 @@ def main():
 
     for url in args.url:
         url = url.rstrip('/')
-        print(url)
         if re.search('/[A-Z][A-Za-z]+Server/[^/]+$', url):
-            query = get_query(url)
-            if query is not None:
-                layer, layer_data, layer_format = query
-                write_layer(layer, layer_data, url, layer_format)
+            downloading(url, metadatas)
         elif re.search('/[A-Z][A-Za-z]+Server$', url):
             for layer_url in get_layers(url):
-                query = get_query(layer_url)
-                if query is not None:
-                    layer, layer_data, layer_format = query
-                    write_layer(layer, layer_data, layer_url, layer_format)
-        # elif re.search('/rest/services$', url):
-        else:
+                downloading(layer_url, metadatas)
+        else:  # elif re.search('/rest/services$', url)
             for service_url in get_services(url):
-                create_time = get_create_time(service_url)  # get create time about this services
-                # if services data is change, downloading, else skipping
-                if check_update(service_url, create_time, metadatas):
-                    for layer_url in get_layers(service_url):
-                        query = get_query(layer_url)
-                        if query is not None:
-                            layer, layer_data, layer_format = query
-                            write_layer(layer, layer_data, layer_url, layer_format)
-                    # update metadata
-                    metadatas[service_url] = create_time
-                else:
-                    print('Skipping - service not update', service_url)
-            save_metadata(metadatas)  # save new metadata
+                for layer_url in get_layers(service_url):
+                    downloading(layer_url, metadatas)
 
-main()
+
+if __name__ == "__main__":
+    # init meta or pass
+    init_metadata()
+    # run main func
+    main()
+    # clear csv
+    clear_metadata()
--- arcgis_dl/metadata.py
+++ arcgis_dl/metadata.py
@@ -1,65 +1,104 @@
 import os.path as osp
 import requests
+import pandas as pd
 import typing
-import pyarrow as pa
-import pyarrow.parquet as pq
-import xml.etree.ElementTree as ET
+import time
 
 
-METADATAS_PATH = "metadata.parquet"
+METADATAS_PATH = "metadata.csv"
 
 
-def get_create_time(url: str) -> str:
-    meta_url = url + "/info/metadata"
-    request = requests.get(meta_url)
-    root = ET.fromstring(request.text)
-    esri = root.find("Esri")
-    if esri is not None:
-        date = esri.find("CreaDate")
-        time = esri.find("CreaTime")
-        if date is not None and time is not None:
-            server_create_time = str(date.text) + str(time.text)
-            return server_create_time
-    return ""
+def _get_date_time(html_str: str) -> str:
+    SSTR = "<b>Last Edit Date:</b>"
+    ESTR = "<br/>"
+    idx_start = html_str.find(SSTR)
+    if idx_start != -1:  # Last Edit Date is find
+        idx_start = html_str.find(SSTR) + len(SSTR)
+        idx_end = idx_start + html_str[idx_start: ].find(ESTR)
+        date_time = html_str[idx_start: idx_end].strip()
+    else:  # Last Edit Date not find
+        tm = time.localtime(time.time())
+        ymd = "/".join([str(tm.tm_mon), str(tm.tm_mday), str(tm.tm_year)])
+        hms = ":".join([str(tm.tm_hour if tm.tm_hour < 12 else tm.tm_hour - 12), str(tm.tm_min), str(tm.tm_sec)])
+        ap = "AM" if tm.tm_hour < 12 else "PM"
+        date_time = " ".join([ymd, hms, ap])
+    return date_time
 
 
-def check_update(url: str, create_time: str, metadatas: typing.Dict) -> bool:
-    if url in metadatas.keys() and create_time == metadatas[url]:
+def get_date_time(layer_url: str) -> str:
+    request = requests.get(layer_url)
+    date_time = _get_date_time(request.text)
+    return date_time
+
+
+def init_metadata(meta_path: str = METADATAS_PATH) -> None:
+    if osp.exists(meta_path):
+        pass
+    else:
+        columns = ["LayerLink", "LastEditDate"]
+        pd.DataFrame(data=None, columns=columns).to_csv(meta_path, index=False, mode="w")
+
+
+def save_metadata(meta_dict: typing.Dict, 
+                  meta_path: str = METADATAS_PATH) -> None:
+    data_list = []
+    for k, v in meta_dict.items():
+        data_list.append([k, v])
+    columns = ["LayerLink", "LastEditDate"]
+    pdd = pd.DataFrame(data=data_list, columns=columns)
+    pdd.to_csv(meta_path, header=False, index=False, mode="a")
+
+
+def _df2dict(df: pd.DataFrame) -> typing.Dict:
+    keys = []
+    values = []
+    for link in df["LayerLink"]:
+        keys.append(link)
+    for date in df["LastEditDate"]:
+        values.append(date)
+    meta_dict = dict(zip(keys, values))
+    return meta_dict
+
+
+def load_metadata(meta_path: str = METADATAS_PATH) -> typing.Dict:
+    pdd = pd.read_csv(meta_path)
+    # duplicate values keep the latest
+    pdd.drop_duplicates(subset=["LayerLink"], keep="last", inplace=True)
+    return _df2dict(pdd)
+
+
+def clear_metadata(meta_path: str = METADATAS_PATH) -> None:
+    if osp.exists(meta_path):
+        pdd = pd.read_csv(meta_path)
+        # duplicate values keep the latest
+        pdd.drop_duplicates(subset=["LayerLink"], keep="last", inplace=True)
+        pdd.to_csv(meta_path, index=False, mode="w")
+
+
+def check_update(layer_url: str, date_time: str, metadatas: typing.Dict) -> bool:
+    if layer_url in metadatas.keys() and date_time == metadatas[layer_url]:
         return False
     return True
 
 
-def save_metadata(rest_metas: typing.Dict, 
-                  metadata_path: str = METADATAS_PATH) -> None:
-    table = pa.Table.from_pydict(rest_metas)
-    pq.write_table(table, metadata_path, compression="GZIP")
-
-
-def load_metadata(metadata_path: str = METADATAS_PATH) -> typing.Dict:
-    if osp.exists(metadata_path):
-        table = pq.read_table(metadata_path)
-        rest_metas = table.to_pydict()
-        for k, v in rest_metas.items():
-            rest_metas[k] = "".join(v)
-        return rest_metas
-    else:
-        return {}
-
-
 if __name__ == "__main__":
-    # # test
-    # rest_metas = {
-    #     "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Energy/Geology/FeatureServer": "2018020817395100",
-    #     "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Energy/Geology/MapServer": "2018020817395100",
-    #     "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Energy/HSEC/FeatureServer": "2018020818053900",
-    #     "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Energy/HSEC/MapServer": "2018020818053900",
-    #     "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Energy/Infrastructure/FeatureServer": "2018020818461800",
-    #     "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Energy/Infrastructure/MapServer": "2018020818461800"
-    # }
-    test_url = "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Energy/Geology/FeatureServer"
-    create_time = get_create_time(test_url)
-    print(create_time)
-    # save_metadata(rest_metas)
-    metadatas = load_metadata()
-    print(metadatas)
-    print(check_update(test_url, create_time, metadatas))
+    # test get time
+    test_url_1 = "https://services6.arcgis.com/bKYAIlQgwHslVRaK/ArcGIS/rest/services/CasesByGovernoratesViewLayer/FeatureServer/0"
+    test_url_2 = "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Energy/Geology/FeatureServer"
+    time_1 = get_date_time(test_url_1)
+    time_2 = get_date_time(test_url_2)
+    print(time_1 + "\n" + time_2)  # time_1 is fixed and time_2 is real time
+    # test save csv
+    test_dict = {
+        test_url_1: time_1,
+        test_url_2: time_2
+    }
+    init_metadata()
+    save_metadata(test_dict)
+    clear_metadata()
+    # test read csv
+    meta_dict = load_metadata()
+    print(meta_dict)  # should be {layer_link: date_time}
+    # test check
+    print(check_update(test_url_1, get_date_time(test_url_1), meta_dict))  # should be False
+    print(check_update(test_url_2, get_date_time(test_url_2), meta_dict))  # should be True
