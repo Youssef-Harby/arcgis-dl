1b82ee5e6b0744399926816c2234673f9938b476
Youssef-Harby <youssef_harby@yahoo.com>
2022-06-19 14:21:09 +0200

    datetime edit

--- Home.py
+++ Home.py
@@ -1,11 +1,32 @@
 import streamlit as st
+from arcgis_dl.__main__ import downloading
 from arcgis_dl.arcgis_dl import config, get_services, get_layers, get_query, write_layer
 from arcgis_dl.df_selection_table import aggrid_interactive_table
-from arcgis_dl.metadata import load_metadata, check_update, save_metadata
-from st_aggrid import AgGrid, GridOptionsBuilder
-from st_aggrid.shared import GridUpdateMode
+from arcgis_dl.metadata import (
+    init_metadata, save_metadata, load_metadata, clear_metadata, \
+    get_date_time, convet_time, check_update
+)
 import pandas as pd
 import re
+try:
+    metadatas = load_metadata()
+    st.write(metadatas)
+except:
+    pass
+
+
+def downloading(url, time_str, metadatas):
+    query = get_query(url)
+    if query is not None:
+        layer, layer_data, layer_format = query
+        data_time = convet_time(time_str)
+        if data_time == -1:
+            data_time = get_date_time(url)
+        if check_update(url, data_time, metadatas):
+            write_layer(layer, layer_data, url, layer_format)
+            save_metadata({url: data_time})
+        else:
+            print('Skipping - no update:', url)
 
 st.title('ArcGIS Server Downloader')
 arc_url = st.text_input('ArcGIS Server Url','https://sampleserver6.arcgisonline.com/arcgis/rest/services/Energy',placeholder='Server or Folder or Layer')
@@ -17,36 +38,16 @@ user_input_date = st.date_input(
 st.write('Your selected comparing date is:', user_input_date)
 
 if st.button('Download'):
-    metadatas = load_metadata()
     url = arc_url.rstrip('/')
-    print(url)
     if re.search('/[A-Z][A-Za-z]+Server/[^/]+$', url):
-        query = get_query(url)
-        if query is not None:
-            layer, layer_data, layer_format = query
-            write_layer(layer, layer_data, url, layer_format)
+        downloading(url, str(user_input_date),metadatas)
     elif re.search('/[A-Z][A-Za-z]+Server$', url):
         for layer_url in get_layers(url):
-            query = get_query(layer_url)
-            if query is not None:
-                layer, layer_data, layer_format = query
-                write_layer(layer, layer_data, layer_url, layer_format)
-    # elif re.search('/rest/services$', url):
-    else:
+            downloading(layer_url, str(user_input_date),metadatas)
+    else:  # elif re.search('/rest/services$', url)
         for service_url in get_services(url):
-            create_time = check_update(service_url)  # get create time about this services
-            # if services data is change, downloading, else skipping
-            if check_update(service_url, create_time, metadatas):
-                for layer_url in get_layers(service_url):
-                    query = get_query(layer_url)
-                    if query is not None:
-                        layer, layer_data, layer_format = query
-                        write_layer(layer, layer_data, layer_url, layer_format)
-                # update metadata
-                metadatas[service_url] = create_time
-            else:
-                print('Skipping - service not update', service_url)
-        save_metadata(metadatas)  # save new metadata
+            for layer_url in get_layers(service_url):
+                downloading(layer_url, str(user_input_date),metadatas)
 
 
 meta_df = pd.read_csv('metadata.csv')
